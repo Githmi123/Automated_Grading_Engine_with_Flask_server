94c27890635268fd7c059730811bd25d318a3f54
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-03-23 23:15:18 +0530

    Added a utility function for fetching image URLs from AWS S3 bucket.

---
+++ flask-server/aws/aws_utils.py
@@ -0,0 +1,18 @@
+import boto3
+
+s3_client = boto3.client('s3')
+
+def fetch_image_urls_from_s3(bucket_name):
+    response = s3_client.list_objects_v2(Bucket = bucket_name)
+    urls = []
+    for obj in response.get('Contents', []):
+        url = s3_client.generate_presigned_url(
+            'get_object',
+            Params = {'Bucket': bucket_name, 'Key': obj['Key']},
+            ExpiresIn = 3600
+        )
+
+        urls.append(url)
+
+    return urls
+
